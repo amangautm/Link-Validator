a0d70e0d54b9d2aad4415c4c5f5e5ac3a8bb24dc
Aman Gautam <iitianamangautam@gmail.com>
2020-06-22 22:35:13 +0530

    Validating through ASynchronous request

--- src/LinkValidatorSync.java
+++ src/LinkValidatorSync.java
@@ -5,6 +5,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class LinkValidatorSync {
 
@@ -12,13 +14,28 @@ public class LinkValidatorSync {
 
     public static void main(String args[]) throws IOException {
 
-        client =HttpClient.newHttpClient();
+        client = HttpClient.newHttpClient();
 
+        /* for synchronous request
         Files.lines(Path.of("urls.txt"))
                 .map(LinkValidatorSync::validateLink)
                 .forEach(System.out::println);
+         */
+
+        //firstly we're going to split performing the requests and handling the CompletableFutures
+        var futures = Files.lines(Path.of("urls.txt"))    //mapping the requests to variable futures
+                .map(LinkValidatorSync::validateLink)
+                .collect(Collectors.toList());
+        //the generated list of requests should be executed in parallel
+
+        //our program doesn't ends before the completableFutures are asynchronously completed.
+        futures.stream()
+                .map(CompletableFuture::join)  //done by join method
+                .forEach(System.out::println);
+        //we end up having with is stream of status strings for all the requests that have been performed.
     }
 
+            /* for synchronous request
     private static String validateLink(String link){
         HttpRequest request =HttpRequest.newBuilder(URI.create(link))
                 .GET()
@@ -32,6 +49,18 @@ public class LinkValidatorSync {
             return String.format("%s -> %s",link,false);
         }
     }
+    */
+
+    private static CompletableFuture<String> validateLink(String link){
+                HttpRequest request =HttpRequest.newBuilder(URI.create(link))
+                        .GET()
+                        .build();
+
+                return client.sendAsync(request,HttpResponse.BodyHandlers.discarding()) //CompletableFuture wrapped with response
+                        .thenApply(LinkValidatorSync::responseToString)//completableFuture coming out from thenApply will be wrapped with string
+                        .exceptionally(e ->String.format("%s -> %s",link,false)); //in case something goes wrong
+    }
+    //thenApply method only defines the action to be taken when CompletableFuture is completed successfully.
 
     private static String responseToString(HttpResponse<Void> response){
         int status =response.statusCode();
